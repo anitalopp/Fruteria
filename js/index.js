d1694880de122decbe4314c732accf84fccaad73
anitalopp <analopezjuan05@gmail.com>
2024-10-08 13:55:16 +0200

    insertar precios y frutas en el js

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const cantidades = {
+    pitahaya: 0,
+    chirimoya: 0,
+    longan: 0,
+    carambola: 0,
+    kiwano: 0,
+    maracuya: 0,
+    lichi: 0,
+    physalis: 0,
+    kumquat: 0,
+    pawpaw: 0,
+};
+
+var dineroGastado = 0;
+
+var precios = {
+    pitahaya: 9.15,
+    chirimoya: 8.60,
+    longan: 7.42,
+    carambola: 6.70,
+    kiwano: 8.99,
+    maracuya: 5.22,
+    lichi: 7.66,
+    physalis: 10.32,
+    kumquat: 8.20,
+    pawpaw: 10.66,
+}
+
+
